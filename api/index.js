9a850257127df119e5b001deedc73b1b39c0d1e0
Derek Clair <derek@derekclair.com>
2017-03-05 15:18:36 -0700

    EDIT:

--- api/index.js
+++ api/index.js
@@ -49,7 +49,10 @@ function findSenatorsByState(req, res, next) {
 
 app.get('/senators/:state', findSenatorsByState, jsonResponse);
 
-app.use('/*', files);
+app.get('*', function(req, res) {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
+
 app.set('port', (process.env.PORT || 3001));
 
 const server = app.listen(app.get('port'), function () {
